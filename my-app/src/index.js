a83981c2248f203669590306909cf9159e3b4873
NelKai <55228995+NelKai@users.noreply.github.com>
2021-04-29 17:45:18 +0300

    Päivitys

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,61 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
-
-/*
-function HaeJSON() {
-  const [leffat, setLeffat] = useState([]);
-  const HaeLeffat = () => {
-    fetch('./leffat.json')
-    .then(function(response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function(leffaJSON) {
-      console.log(leffaJSON);
-      setLeffat(leffaJSON)
-    });
-  }
-  useEffect(() => {
-    HaeLeffat();
-  }, []);
-
-  return (
-    <div className="leffat">
-      {leffat && leffat.length > 0 && leffat.map((item, i) => <p key={i}>{item.about}</p>)}
-    </div>
-  )
-}
-
-export default HaeJSON;
-
-ReactDOM.render(<HaeJSON />, document.getElementById('kaikki'));
-*/
-
-/*
-import data from './leffat.json';
-
-const leffaJSON = data.map((data) => {
-  return (
-    <ul>
-      <li key={data.id}>{data.nimi}</li>
-      <li>{data.ohjaaja}</li>
-    </ul>
-  )
-})
-
-class HaeKaikki extends React.Component {
-  render() {
-    return 
-  }
-}
-
-ReactDOM.render(<HaeKaikki />, document.getElementById('kaikki'));
-*/
-
-
 import leffat from './leffat.json';
-
 
 const LeffaArray = (props) => {
   const {data} = props;
@@ -88,55 +34,6 @@ const LeffaArray = (props) => {
  )
 };
 
-
-/*
-function leffaArray(props) {
-  const {data} = props;
-
-  return (
-    <div>
-      <ul>
-        {data.map((item) => (
-          <li>
-            {item.nimi} ({item.ohjaaja})
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-};
-*/
-
-/*
-class Etusivu extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Tervetuloa!</h1>
-        <form>
-          <div className="form-group">
-            <label>Hae tiettyä elokuvaa:</label>
-            <input
-              type="search"
-              className="form-control"
-              placeholder="Syötä nimi (esim. Avatar)"
-            />
-            <div className="form-group">
-              <button type="submit" className="btn">Hae</button>
-              <button type="button" className="btn">Hae kaikki elokuvat</button>
-            </div>
-          </div>
-        </form>
-        <div id='elokuvat'>
-          <LeffaArray data={leffat} />
-        </div>
-      </div>
-    );
-  }
-};
-//<LeffaArray data={leffat} />
-*/
-
 class EtusivuKo extends React.Component {
   render() {
     return (
@@ -159,54 +56,3 @@ class EtusivuKo extends React.Component {
 }
 
 ReactDOM.render(<EtusivuKo />, document.getElementById('etusivu'));
-
-/*
-function HaeKaikki() {
-  const [leffat, setLeffat] = useState([])
-  fetch("http://localhost:3001/api/getall")
-    .then((leffat) => {
-      return leffat.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const items = data;
-
-      setLeffat(items);
-    });
-};
-
-const LeffaArray = (props) => {
-  const {data} = props;
-
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr key={props.id}>
-            <th scope="col">Nimi</th>
-            <th scope="col">Ohjaaja</th>
-            <th scope="col">Julkaisuvuosi</th>
-            <th scope="col">Genre</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, i) => (
-            <tr>
-              <td key={i}>{item.nimi}</td>
-              <td>{item.ohjaaja}</td>
-              <td>{item.jvuosi}</td>
-              <td>{item.genre}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-const handleClick = (event) => {
-  event.preventDefault();
-  
-  HaeKaikki();
-}
-*/
